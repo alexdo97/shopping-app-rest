9f02cfd8ac226447d4e2902f415bee3eff90a9f4
Alexandru Dobrin <alexdo97@yahoo.com>
2022-06-05 21:44:42 +0300

    Added identity model

---
+++ src/main/java/com/alexdo97/model/Identity.java
@@ -0,0 +1,67 @@
+package com.alexdo97.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "identity")
+public class Identity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String username;
+
+	@JsonIgnore
+	private String password;
+
+	@OneToOne(mappedBy = "identity", cascade = CascadeType.ALL)
+	@PrimaryKeyJoinColumn
+	private Customer customer;
+
+	public Identity() {
+
+	}
+
+	public Identity(String username, String password, Customer customer) {
+		this.username = username;
+		this.password = password;
+		this.customer = customer;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
