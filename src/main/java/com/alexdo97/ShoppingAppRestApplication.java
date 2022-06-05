41675b37c4fea0e3518cd01e01f0c22c45b6b372
Alexandru Dobrin <alexdo97@yahoo.com>
2022-06-05 19:16:26 +0300

    Plain Spring boot app

---
+++ src/main/java/com/alexdo97/ShoppingAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.alexdo97;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAppRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexdo97/shoppingapprest/ShoppingAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexdo97.shoppingapprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
