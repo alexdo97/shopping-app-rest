0d3a5785db0a9a70c6e900f39e2f6f75ef71057a
Alexandru Dobrin <alexdo97@yahoo.com>
2022-06-13 20:21:32 +0300

    Added new exception: AuthenticatedPrincialException

---
+++ src/main/java/com/alexdo97/exception/AuthenticatedPrincialException.java
@@ -0,0 +1,14 @@
+package com.alexdo97.exception;
+
+public class AuthenticatedPrincialException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AuthenticatedPrincialException(String errorMessage) {
+		super(errorMessage);
+	}
+
+}
